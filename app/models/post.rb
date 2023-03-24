bf3db9d63eb5251863739c7c744c62dbc606daa7
Caroline Syokau <caroline.syokau@student.moringaschool.com>
2023-03-24 11:01:22 +0300

    completed assinment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
+class ClickbaitValidator < ActiveModel::Validator
+    def validate(record)
+      unless record.title =~ /(Won't Believe|Secret|Top \d|Guess)/
+        record.errors[:title] << "must be clickbait-y"
+      end
+    end
+  end
+  
 class Post < ApplicationRecord
-end
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  end
